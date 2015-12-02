eabe62ddcd0a1c6de482e5f0ea91b346da6c1277
Tsubasa HIRANO <hirano.tsubasa@imagica.jp>
2015-12-02 17:29:08 +0900

    Initial commit

---
+++ SparkleSample/AppDelegate.cs
@@ -0,0 +1,30 @@
+﻿using AppKit;
+using Foundation;
+using Sparkle;
+using System;
+
+namespace SparkleSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            var updater = SUUpdater.SharedUpdater;
+            updater.FeedURL = new NSUrl();
+            updater.AutomaticallyChecksForUpdates = true;
+            updater.UpdateCheckInterval = TimeSpan.FromDays(1).TotalSeconds;
+            updater.CheckForUpdates(this);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
+
---
+++ SparkleSample/Main.cs
@@ -0,0 +1,28 @@
+﻿using AppKit;
+using System;
+using System.IO;
+using System.Reflection;
+using ObjCRuntime;
+
+namespace SparkleSample
+{
+    static class MainClass
+    {
+        static string GetCurrentExecutingDirectory()
+        {
+            string filePath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+            return Path.GetDirectoryName(filePath);
+        }
+
+        static void Main(string[] args)
+        {
+            if (Dlfcn.dlopen(Path.Combine(GetCurrentExecutingDirectory(), "Sparkle"), 0) == IntPtr.Zero)
+            {
+                Console.Error.WriteLine("Unable to load the dynamic library.");
+                Environment.Exit(1);
+            }
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
